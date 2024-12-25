bd9b6a46453b16714118c1bc239d2f2352c23c5f
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2024-12-25 18:05:35 -0300

    Add elaped time and make spaceship move left and right

--- src/game.h
+++ src/game.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "sdl2_backend.h"
+#include "vec2.h"
 
 class Game
 {
@@ -19,8 +20,12 @@ public:
     _is_running = true;
     SDL_Event e;
     
+    Uint32 elapsed_time = 0;
     while(_is_running)
     {
+      Uint32 start_time = SDL_GetTicks();  // Record the start time
+      int last_time = start_time;
+      
       while (SDL_PollEvent(&e) != 0)
       {
         if(e.type == SDL_QUIT)
@@ -28,20 +33,37 @@ public:
           _is_running = false;
         }
       }
-      update();
+      
+      update(elapsed_time / 1000.0f);
       draw();
+      
+      Uint32 current_time = SDL_GetTicks();
+      elapsed_time = current_time - start_time;
     }
   }
 private:
   SDL2Backend _backend;
   bool _is_running;
   
+  //SpaceShip
+  Vec2 _spaceship_pos = {0,0};
+  Vec2 _spaceship_size = {256,256};
+  float _spaceship_speed = 100.f;
+  
   void init()
   {
   }
-  void update()
+  void update(float elapsed_time)
   {
-  
+    const Uint8* keystates = SDL_GetKeyboardState(NULL);
+    if(keystates[SDL_SCANCODE_RIGHT])
+    {
+      _spaceship_pos.x += _spaceship_speed * elapsed_time;
+    }
+    if(keystates[SDL_SCANCODE_LEFT])
+    {
+      _spaceship_pos.x -= _spaceship_speed * elapsed_time;
+    }
   }
   void draw()
   {
@@ -52,7 +74,9 @@ private:
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-    SDL_Rect fillRect = {0, 0, 256, 256};
+    SDL_Rect fillRect = {(int)_spaceship_pos.x,(int)_spaceship_pos.y,
+                         (int)_spaceship_size.x,(int)_spaceship_size.y};
+    
     SDL_RenderFillRect(renderer, &fillRect);
 
     SDL_RenderPresent(renderer);
