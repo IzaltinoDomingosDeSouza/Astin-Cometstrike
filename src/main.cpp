fc74263a80c89f7a682a27f75c5cbf901a2b5789
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2024-12-25 17:32:32 -0300

    create a simple sdl2 backend

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,14 @@
-#include <SDL2/SDL.h>
 #include <fmt/core.h>
+
+#include "sdl2_backend.h"
 
 int main(int argc, char * argv[])
 {
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        fmt::print("SDL could not initialize! SDL_Error : {}\n", SDL_GetError());
-        return 1;
-    }
-    SDL_Window * window = SDL_CreateWindow("SDL2 Rectangle Example",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          800, 600,
-                                          SDL_WINDOW_SHOWN);
-    if(!window)
-    {
-        fmt::print("Window could not be created! SDL_Error: {}\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer)
-    {
-        fmt::print("Renderer could not be created! SDL_Error : {}\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+  
+    SDL2Backend backend;
+    backend.init("Atsin Comestrike",800,600,true);
+    
+    auto renderer = backend.get_renderer();
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -40,9 +21,5 @@ int main(int argc, char * argv[])
     SDL_RenderPresent(renderer);
 
     SDL_Delay(5000);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 }
 
---
+++ src/sdl2_backend.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <fmt/core.h>
+
+class SDL2Backend
+{
+public:
+  SDL2Backend()
+  {
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+      fmt::print("SDL could not initialize! SDL_Error : {}\n", SDL_GetError());
+    }
+  }
+  ~SDL2Backend()
+  {
+    if(!_renderer) SDL_DestroyRenderer(_renderer);
+    if(!_window) SDL_DestroyWindow(_window);
+    
+    SDL_Quit();
+  }
+  void init(const char * title,size_t width,size_t height,bool fullscreen = false)
+  {
+    _window = SDL_CreateWindow(title,
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              width, height,
+                              fullscreen ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_SHOWN);
+    if(!_window)
+    {
+        fmt::print("Window could not be created! SDL_Error: {}\n", SDL_GetError());
+        SDL_Quit();
+        return;
+    }
+    _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
+    if(!_renderer)
+    {
+        fmt::print("Renderer could not be created! SDL_Error : {}\n", SDL_GetError());
+        SDL_DestroyWindow(_window);
+        SDL_Quit();
+        return;
+    }
+  }
+  SDL_Renderer * get_renderer()
+  {
+    return _renderer;
+  }
+private:
+  SDL_Window * _window = nullptr;
+  SDL_Renderer * _renderer = nullptr;
+};
